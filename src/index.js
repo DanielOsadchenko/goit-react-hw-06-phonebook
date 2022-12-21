f6c121e2f866da7d7c9c2662b8ca2405c7bd92c3
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-12-21 18:35:08 +0100

    persist completed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+import { store, persistor } from './redux/store'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}></PersistGate>
     <Provider store={store}>
       <App />
     </Provider>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  phonebookReducer  from "./phonebookSlice";
+import phonebookReducer from "./phonebookSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts']
+}
+
+const persistedReducer = persistReducer(persistConfig, phonebookReducer)
 
 export const store = configureStore({
   reducer: {
-    phonebook: phonebookReducer,
+    phonebook: persistedReducer,
+    
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store)
